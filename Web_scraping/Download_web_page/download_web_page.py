cf37aed81553db1990a648ba3b2c25cfb6605855
uttamn <uttam.jn@gmail.com>
2018-10-12 02:20:39 +0530

    Update download_web_page.py

--- Web_scraping/Download_web_page/download_web_page.py
+++ Web_scraping/Download_web_page/download_web_page.py
@@ -1,1 +1,15 @@
- 
+ #downloading web pages 
+
+import requests
+url = 'http://www.columbia.edu/~fdc/sample.html'
+response = requests.get(url)
+response.status_code
+response.text
+response.request.headers
+
+response.request
+
+response.request.url
+
+#Check web download status
+#requests.get('http://www.columbia.edu/invalid')
